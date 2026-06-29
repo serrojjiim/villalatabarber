4f11388177833e1ba5704478967a0d5d69ad0a6e
serrojjiim <serrojjim@gmail.com>
2026-06-29 15:18:20 +0200

    euro

--- components/PanelAjustes.tsx
+++ components/PanelAjustes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Settings, DollarSign, euro } from 'lucide-react';
+import { Settings, DollarSign, Euro } from 'lucide-react';
 
 interface Ajustes {
   id? : number;
